85303a1b5ecc32f817bf813d7f5deebc1a521f5a
Raymond <iamraymondtieu@gmail.com>
2016-12-08 02:46:18 -0500

    fixed 13x13 and 15x30 board values

--- test_cases.py
+++ test_cases.py
@@ -80,7 +80,7 @@ def test_full_run(model, stu_orderings, name=""):
                  [(0,7),None,None,None],
                  [(0,10),None,None,None]]
 
-        # 9x8
+        # # 9x8
         board = [[0, 0, (22,0), (15,0), 0, 0, 0, 0],
                  [0, (3,7), None, None, (5,0), 0, 0, 0],
                  [(0,18), None, None, None, None, (13,0), 0, 0],
@@ -113,21 +113,21 @@ def test_full_run(model, stu_orderings, name=""):
                  [0,0,0,(0,10),None,None,0,0]
                 ]
         
-        # 13x13
-        # board = [[0,0,0,(3,0),(10,0),0,(6,0),(16,0),(15,0),0,(16,0),(15,0),0],
-        #          [0,0,(0,4),None,None,(0,14),None,None,None,(3,8),None,None,0],
-        #          [0,0,(0,3),None,None,(3,26),None,None,None,None,None,None,(17,0)],
-        #          [0,(17,0),(4,0),(0,9),None,None,None,(30,6),None,None,(0,11),None,None],
-        #          [(0,10),None,None,(11,3),None,None,(0,8),None,None,0,(10,13),None,None],
-        #          [(0,12),None,None,None,(16,0),0,(29,11),None,None,(17,7),None,None,0],
-        #          [0,0,(0,11),None,None,(0,15),None,None,(0,11),None,None,0,0],
-        #          [0,0,(34,10),None,None,(34,14),None,None,(0,9),None,None,(4,0),(3,0)],
-        #          [0,(17,11),None,None,(0,17),None,None,0,(16,0),(11,8),None,None,None],
-        #          [(0,13),None,None,0,(3,11),None,None,(23,8),None,None,(0,3),None,None],
-        #          [(0,17),None,None,(17,8),None,None,(3,22),None,None,None,(17,0),0,0],
-        #          [0,(0,31),None,None,None,None,None,None,(0,12),None,None,0,0],
-        #          [0,(0,17),None,None,(0,17),None,None,None,(0,10),None,None,0,0]
-        #         ]
+        # # 13x13
+        board = [[0,0,0,(3,0),(10,0),0,(6,0),(16,0),(15,0),0,(16,0),(15,0),0],
+                 [0,0,(0,4),None,None,(0,14),None,None,None,(3,8),None,None,0],
+                 [0,0,(0,3),None,None,(3,26),None,None,None,None,None,None,(17,0)],
+                 [0,(17,0),(4,0),(0,9),None,None,None,(30,6),None,None,(0,11),None,None],
+                 [(0,10),None,None,(11,3),None,None,(0,8),None,None,0,(10,13),None,None],
+                 [(0,12),None,None,None,(16,0),0,(29,11),None,None,(17,7),None,None,0],
+                 [0,0,(0,11),None,None,(0,15),None,None,(0,11),None,None,0,0],
+                 [0,0,(34,10),None,None,(34,14),None,None,(0,9),None,None,(4,0),(3,0)],
+                 [0,(17,11),None,None,(0,17),None,None,0,(16,0),(11,8),None,None,None],
+                 [(0,13),None,None,0,(3,11),None,None,(23,8),None,None,(0,3),None,None],
+                 [(0,17),None,None,(17,8),None,None,(3,22),None,None,None,(17,0),0,0],
+                 [0,(0,31),None,None,None,None,None,None,(0,12),None,None,0,0],
+                 [0,(0,17),None,None,(0,17),None,None,None,(0,10),None,None,0,0]
+                ]
         
         # board = [[0,(4,0),(23,0),0,0,(17,0),(3,0),0,0,0,0,0,0],
         #          [(0,12),None,None,(4,0),(28,9),None,None,0,0,0,0,0,0],
@@ -138,7 +138,7 @@ def test_full_run(model, stu_orderings, name=""):
         #          [0,(24,16),None,None,None,(17,27),None,None,None,None,(4,8),None,None],
         #          [(0,16),None,None,(3,23),None,None,None,None,(4,12),None,None,None,0],
         #          [(0,25),None,None,None,None,None,(0,18),None,None,None,None,0,0],
-        #          [(0,19),None,None,None,0,0,0,(0,10),None,None,(3,0),(24,0),None],
+        #          [(0,19),None,None,None,0,0,0,(0,10),None,None,(3,0),(24,0),0],
         #          [0,0,0,0,0,0,0,(4,0),(17,18),None,None,None,(16,0)],
         #          [0,0,0,0,0,0,(0,34),None,None,None,None,None,None],
         #          [0,0,0,0,0,0,(0,9),None,None,0,(0,16),None,None]
@@ -154,7 +154,7 @@ def test_full_run(model, stu_orderings, name=""):
         #          [0, 0, 0, 0, (4,0), (23,35), None, None, None, None, None, None, (0,15), None, None, None, 0, 0, (0,9), None, None, (23,15), None, None, None, None, (3,6), None, None, None],
         #          [0, (17,0), (16,0), (0,13), None, None, None, (23,0), (4,10), None, None, 0, 0, (0,16),None, None, (17,0), 0, 0, (0,21), None, None, None, None, (17,3), None, None, (0,4), None, None],
         #          [(0,16), None, None, (3,7),None, None, (13,25), None, None, None, None, (3,0), 0, 0, (0,15), None, None, (10,0), 0, (4,17), None, None, (3,0), (16,12), None, None, None, 0, 0, 0],
-        #          [(0,19),None, None, None, (11,17), None, None, None, None, (0,4), None, None, (17,0), None, (0,18), None, None, None, (17,32), None, None, None, None, None, None, 0, 0, 0, 0, 0],
+        #          [(0,19),None, None, None, (11,17), None, None, None, None, (0,4), None, None, (17,0), 0, (0,18), None, None, None, (17,32), None, None, None, None, None, None, 0, 0, 0, 0, 0],
         #          [0, 0, (0,4), None, None, (24,11), None, None, 0, (29,17), None, None, None, (10,0), 0, 0, (0,20), None, None, None, None, (0,3), None, None, (3,0), 0, 0, (23,0), (11,0), 0],
         #          [0, 0, (6,0), (23,16), None, None, None, 0, (0,16), None, None, (0,10), None, None, (16,0), 0, (24,12), None, None, (6,0), (24,0), 0, (17,8), None, None, (24,0), (0,13), None, None, 0],
         #          [0, (0,26), None, None, None, None, None, 0, (4,9), None, None, (16,0), (3,10), None, None, (4,13),None, None, (0,9), None, None, (4,21), None, None, None, None, (3,8), None, None, (17,0)],
@@ -162,10 +162,10 @@ def test_full_run(model, stu_orderings, name=""):
         #          [0, (0,8), None, None, 0, 0, 0, (0,9), None, None, (0,14), None, None, None, (0,8), None, None, 0, (0,14), None, None, None, 0, 0, (0, 10), None, None, (0,12), None, None]
         #         ]
 
-        csp,var_array = model(board)
-        solver = BT(csp)
-        solver.bt_search(prop_BT, stu_orderings.ord_mrv, stu_orderings.val_arbitrary)
-        print(check_solution(board, csp.get_all_cons()))
+#        csp,var_array = model(board)
+#        solver = BT(csp)
+#        solver.bt_search(prop_BT, stu_orderings.ord_mrv, stu_orderings.val_arbitrary)
+#        print(check_solution(board, csp.get_all_cons()))
 
         csp,var_array = model(board)
         print("===== FC ====")
